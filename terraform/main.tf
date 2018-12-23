19abae87ad15d6b02eb7ff10f9ee6156ba94ec84
Anton Khayrutdinov <nemanovich@gmail.com>
2018-12-23 23:10:38 +0000

    [terraform-1] add input vars and ssh keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,62 @@
 provider "google" {
   version = "1.4.0"
-  project = "infra-221016"
-  region = "europe-west1"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_compute_project_metadata" "ssh_keys" {
+  metadata {
+    ssh-keys = <<EOF
+otus:${var.public_key} otus
+appuser1:${var.public_key} appuser1
+appuser2:${var.public_key} appuser2
+EOF
+  }
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
-  tags = ["reddit-app"]
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
   boot_disk {
     initialize_params {
-      image = "reddit-base"
+      image = "${var.disk_image}"
     }
   }
+
   network_interface {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config = {}
   }
-  metadata {
-    ssh-keys = "otus:${file("~/.ssh/otus.pub")}"
-  } 
+
   connection {
-    type = "ssh"
-    user = "otus"
-    agent = false
-    private_key = "${file("~/.ssh/otus")}"
+    type        = "ssh"
+    user        = "otus"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
   }
+
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
-  } 
+  }
+
   provisioner "remote-exec" {
     script = "files/deploy.sh"
-  } 
-} 
+  }
+}
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
+
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
-} 
+  target_tags   = ["reddit-app"]
+}
